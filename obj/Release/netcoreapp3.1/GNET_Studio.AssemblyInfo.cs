89f1937267f7911f5538d618377d77a918333d75
Alex T <a_talamonti@hotmail.com>
2020-04-21 00:12:11 -0400

    I think I changed something but visual studio might be nutty

--- obj/Release/netcoreapp3.1/GNET_Studio.AssemblyInfo.cs
+++ obj/Release/netcoreapp3.1/GNET_Studio.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GNET_Studio")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GNET_Studio")]
